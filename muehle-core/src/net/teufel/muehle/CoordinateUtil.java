7ac0d6859eb8f96bbc788469c1bde14bd79db0f9
Timo Teufel <teufel.timo@gmail.com>
2013-10-26 19:54:39 +0200

    X und Y begrenzt

--- muehle-core/src/net/teufel/muehle/CoordinateUtil.java
+++ muehle-core/src/net/teufel/muehle/CoordinateUtil.java
@@ -10,12 +10,20 @@ public class CoordinateUtil {
 			ret = " links";
 		if(x == 4)
 			ret = " mitte";
+		if(x > 7)
+			return ret = " Falsche Eingabe";
+		if(x < 1)
+			return ret = " Falsche Eingabe";
 		if(y > 4)
 			ret = ret + " oben";
 		if(y < 4)
 			ret = ret + " unten";
 		if(y == 4)
 			ret = ret + " mitte";
+		if(y > 7)
+			return ret = " Falsche Eingabe";
+		if(y < 1)
+			return ret = " Falsche Eingabe";
 		return ret;
 	}
 	
--- muehle-core/src/net/teufel/muehle/Muehle.java
+++ muehle-core/src/net/teufel/muehle/Muehle.java
@@ -5,7 +5,8 @@ public class Muehle {
 	public static void main(String[] args) {
 		
 		System.out.println( CoordinateUtil.checkCoordinate(4, 6) );
-		System.out.println( CoordinateUtil.checkCoordinate(7, 1) );
+		System.out.println( CoordinateUtil.checkCoordinate(23, 1) );
+		System.out.println( CoordinateUtil.checkCoordinate(5, -6) );
 
 	}
 
